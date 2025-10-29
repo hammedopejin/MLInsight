666a17d097ab9425552d61ae06a35a3927ee1516
blackmagic <hammedopejin@yahoo.com>
2025-10-29 18:18:58 -0500

    Initial Commit

---
+++ MLInsight/MLInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLInsightApp.swift
+//  MLInsight
+//
+//  Created by blackmagic on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MLInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MLInsightTests/MLInsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  MLInsightTests.swift
+//  MLInsightTests
+//
+//  Created by blackmagic on 10/29/25.
+//
+
+import Testing
+@testable import MLInsight
+
+struct MLInsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
